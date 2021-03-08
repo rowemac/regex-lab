5943a833d29cdc5b283d65bee13dca024ec601f1
Mac Rowe <m.rowe42@gmail.com>
2021-03-08 08:44:16 -0500

    commit tree fcb3925d9cb443814a5ad173649e45ef704931c0

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,19 +1,38 @@
-def starts_with_a_vowel?(word)
+require 'pry'
 
+def starts_with_a_vowel?(word)
+    if word.match(/^[aeiou]|^[AEIOU]/)
+        return true
+    else
+        return false
+    end
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-
+    text.scan(/un\w+ing/)
 end
 
 def words_five_letters_long(text)
-
+    new_array = text.split(" ")
+    new_array.grep(/\b\w{5}\b/)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-
+    #binding.pry
+    if text.match(/^[A-Z]/) && text.match(/[\.]$/)
+        return true
+    else
+        return false
+    end
 end
 
 def valid_phone_number?(phone)
-
+    #binding.pry
+    if phone.match(/(\([0-9]{3}\)([0-9]{3}-[0-9]{4})\b)/) ||
+        phone.match(/\b([0-9]{7})\b/) ||
+        phone.match(/([0-9] ?){10}/)
+        true
+    else
+        false
+    end
 end
